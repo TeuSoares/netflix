dd463ce139c7a1e4cfc7e1657ea3484d09e23d55
Mateus Soares <mateussoares114@gmail.com>
2021-04-14 23:12:18 -0300

    Iniciado a formatação

---
+++ perfilAtivo.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Netflix</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <meta name="description" content="Assista filmes e séries em um só lugar">
+    <link rel="shortcut icon" href="img/netflixIcon.png"/>
+</head>
+<body>
+    <div class="logoNetflix">
+        <div class="logo">
+            <a href="#">
+                <img src="img/logoNetflix.png" alt="Logo da Netflix">
+            </a>
+        </div>
+    </div>
+    <div class="perfilAtivo">
+        <h1 class="title-h1">Quem está assistindo?</h1>
+        <div class="links-perfil">
+            <a href="index.php">
+                <div class="perfil"></div>
+                <span class="nomePerfil">Ielson</span>
+            </a>
+            <a href="index.php">
+                <div class="perfil"></div>
+            </a>
+            <a href="index.php">
+                <div class="perfil"></div>
+            </a>
+            <a href="index.php">
+                <div class="perfil"></div>
+            </a>
+        </div>
+        <div class="gerenciar-perfil">
+            <a href="#">GERENCIAR PERFIS</a>
+        </div>
+    </div>
+</body>
+</html>
